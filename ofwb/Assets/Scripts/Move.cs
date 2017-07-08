7fc44b62ea7036444accd4e45a6be27dc2fed90a
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-07-07 21:31:15 -0400

    gaze and click

---
+++ ofwb/Assets/Scripts/Move.cs
@@ -0,0 +1,67 @@
+﻿/*
+Move and/or scale a game object to a desire position and size.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Speed of the movement
+	public float speed = 1.0F;
+
+	// Origin Position
+	public Vector3 originPosition = new Vector3 (0, 0, 0);
+	// Target Position
+	public Vector3 targetPosition = new Vector3 (0, 0, 0);
+
+	// Origin Rotation
+	public Quaternion originRotation = Quaternion.Euler (0, 0, 0);
+	// Target Rotation
+	public Quaternion targetRotation = Quaternion.Euler (0,0,0);
+
+	// Origin Scale
+	public Vector3 originScale = new Vector3 (0, 0, 0);
+	// Target Scale
+	public Vector3 targetScale = new Vector3 (0, 0, 0);
+
+	// Boolean to start the movement
+	private bool start = false;
+	// Boolean to reset the object
+	private bool reset = false;
+
+	// Function to be called when the object is selected (gazed at)
+	void OnSelect() {
+		start = true;
+	}
+
+	// Function to be called when the object is reseted (gazed at)
+	void OnReset() {
+		reset = true;
+	}
+
+	void Update () {
+		// If start is true and the position and rotation is not the desired one, move and rotate using lerp.
+		if(start && transform.position != targetPosition){
+
+			// Move
+			transform.localPosition = Vector3.Lerp (transform.localPosition, targetPosition, speed * Time.deltaTime);
+			// Scale
+			transform.localScale = Vector3.Lerp (transform.localScale, targetScale, speed * Time.deltaTime);
+			// Rotate
+			transform.localRotation = Quaternion.Lerp (transform.localRotation, targetRotation, speed * Time.deltaTime);
+
+		}
+		// If reset is true and the position and rotation is not the origin one, move and rotate using lerp.
+		if(reset && transform.position != originPosition){
+			// Move
+			transform.position = Vector3.Lerp (transform.position, originPosition, speed * Time.deltaTime);
+			// Rotate
+			transform.rotation = Quaternion.Lerp (transform.rotation, originRotation, speed * Time.deltaTime );
+			// Scale
+			transform.localScale = Vector3.Lerp (transform.localScale, originScale, speed * Time.deltaTime);
+
+		}
+	}
+}
---
+++ ofwb/Assets/Scripts/RecreateGaze.cs
@@ -0,0 +1,41 @@
+﻿/*
+This scripts recreates the Hololens Gaze function (GazeGestureManager) with a mouse click.
+Useful when developing without a device at hand.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecreateGaze : MonoBehaviour {
+
+	// Gameobject being clicked (gazed)
+	public GameObject FocusedObject {get; private set;}
+
+	// User
+	public Camera camera;
+
+
+	void Update () {
+		// If the mouse is pressed
+		if (Input.GetMouseButtonDown (0)) {
+			print("mouse clicked");
+			// Create a ray from the current camera to the mouse position
+			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+			// Create an empty RaycastHit element
+			RaycastHit hit;
+			// Check if the ray hits an element. If it does, store the hitted element in the hit variable.
+			if (Physics.Raycast (ray, out hit)) {
+				// Save that object as the FocusedObject
+				FocusedObject = hit.collider.gameObject;
+				//Debug.Log (FocusedObject);
+				// Send a message to that gameObject
+				FocusedObject.SendMessageUpwards ("OnSelect");
+			} else {
+				// If nothing is hit, clear the FocusedObject
+				FocusedObject = null;
+			}
+		}
+
+	}
+}
